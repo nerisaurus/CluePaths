aa1de2968dc9d37e51316a640f100f9d038eda2d
Nathan Neri <nneri@mymail.mines.edu>
2013-09-27 19:12:39 -0600

    Targetz and getAdjList chain of methods complete (?)

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,6 +1,8 @@
 package experiment;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
@@ -9,6 +11,7 @@ public class IntBoard {
 	private Map<Integer, LinkedList<Integer>> adjacencyMatrix;
 	private boolean visited[];
 	private int width;
+	private Set<Integer> currentTargetz;
 
 	public IntBoard(int height, int width) {
 		int boardSize = height * width; //a rectangular board
@@ -22,23 +25,44 @@ public class IntBoard {
 	}
 	
 	public void startTargetz(int x, int y, int steps){
-		
+		currentTargetz = new HashSet<Integer>(); //a blank sheet
+		Arrays.fill(visited,false); //to ensure no issues
+		int start = calcIndex(x,y);
+		visited[start] = true;
+		calcTargetz(start, steps);
+	}
+	
+	private void calcTargetz(int index, int steps) {
+		LinkedList<Integer> adjacent_squares = getAdjList(index);
+		for(int adjacent : adjacent_squares){
+			visited[adjacent] = true;
+			if(steps == 1) {
+				currentTargetz.add(adjacent);
+			} else {
+				calcTargetz(adjacent, steps - 1);
+			}
+		}
+		visited[index] = false;
 	}
 	
 	public int calcIndex(int x, int y) {
-		return (width*y + x); //TODO complete later
+		return (width*y + x);
 	}
 	
 	public Set<Integer> getTargetz(){
 		
-		return null;
-		//TODO
+		return currentTargetz;
 	}
 	
 	public LinkedList<Integer> getAdjList(int index) {//use calcIndex to get the index
+		LinkedList<Integer> adjacent_squares = new LinkedList<Integer>();
 		
-		return null;
-		//TODO
+		for(int square : adjacencyMatrix.get(index)){
+			if(!visited[square]) {
+				adjacent_squares.add(square);
+			}
+		}
+		return adjacent_squares;
 	}
 
 }
