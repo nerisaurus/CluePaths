f60d7efa86526d69d832aadcb3d7e7c5dbe4f783
Nick Wunder <nwunder@mymail.mines.edu>
2013-09-27 19:16:09 -0600

    calcAdj. pushing b'cuz other methods need a linkedlist...

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -9,15 +9,56 @@ public class IntBoard {
 	private Map<Integer, LinkedList<Integer>> adjacencyMatrix;
 	private boolean visited[];
 	private int width;
+	private int height;
+	private int boardSize;
 
 	public IntBoard(int height, int width) {
-		int boardSize = height * width; //a rectangular board
+		this.boardSize = height * width; //a rectangular board
 		adjacencyMatrix = new HashMap<Integer, LinkedList<Integer>>(boardSize);
 		visited = new boolean [boardSize];
 		this.width = width;
+		this.height = height;
+		calcAdjacencies();
 	}
 	
-	public void calcAdjacencies(){
+	//no need to call other than in the constructor.
+	private void calcAdjacencies(){
+		for(int i=0; i<boardSize; i++){
+			LinkedList<Integer> list = new LinkedList<Integer>();
+			
+			//top row
+			if(i<width){
+				list.add(i+1);
+				list.add(i+width);
+				list.add(i-1);
+			}
+			//left column
+			else if(i % height == 0){
+				list.add(i+1);
+				list.add(i+width);
+				list.add(i-width);
+			}
+			//right column
+			else if((i+1) % height ==0) {
+				list.add(i+1);
+				list.add(i+width);
+				list.add(i-width);
+			}
+			//bottom row
+			else if(i > (boardSize-width)){
+				list.add(i+1);
+				list.add(i-width);
+				list.add(i-1);
+			}
+			//middle
+			else {
+				list.add(i+1);
+				list.add(i+width);
+				list.add(i-1);
+				list.add(i-width);
+			}
+			adjacencyMatrix.put(i, list);
+		}
 		
 	}
 	
@@ -26,7 +67,7 @@ public class IntBoard {
 	}
 	
 	public int calcIndex(int x, int y) {
-		return (width*y + x); //TODO complete later
+		return (width*y + x);
 	}
 	
 	public Set<Integer> getTargetz(){
@@ -36,9 +77,7 @@ public class IntBoard {
 	}
 	
 	public LinkedList<Integer> getAdjList(int index) {//use calcIndex to get the index
-		
-		return null;
-		//TODO
+		return adjacencyMatrix.get(index);
 	}
 
 }
